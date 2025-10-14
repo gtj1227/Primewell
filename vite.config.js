853f5138316325913870aeed78f6b07e1ae7fae0
gtj1227 <gtjereos@gmail.com>
2025-10-14 13:43:18 -0400

    Clean-up

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,9 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  assetsInclude: ["**/*.PNG"] //allows for PNG images retrieval for vite
+  assetsInclude: ["**/*.PNG"], //allows for PNG images retrieval for vite
+  server: {
+    host: '0.0.0.0',
+    port: 5173 // you can change this if needed
+  }
 })
