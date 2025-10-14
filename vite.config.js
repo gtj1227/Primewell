9ed63ade76b5f276cd82e216b961b78428d01126
gtj1227 <126986839+gtj1227@users.noreply.github.com>
2025-10-14 13:30:43 -0400

    Add files via upload

---
+++ vite.config.js
@@ -0,0 +1,8 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+// https://vite.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  assetsInclude: ["**/*.PNG"] //allows for PNG images retrieval for vite
+})
